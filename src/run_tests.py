8f32c35a5f77f2ccaca30a53dce455af0fe083a0
Semyon Zernov <st095228@student.spbu.ru>
2023-07-10 01:13:08 +0300

    SRC: change tab to 4 whitespaces according to pip8 (2)

--- src/run_tests.py
+++ src/run_tests.py
@@ -17,31 +17,31 @@ extDirs = os.listdir(TMP_DIR)
 
 @dataclass
 class Statistics:
-	test_counter: int = 0
-	passed_counter: int = 0
+    teest_counter: int = 0
+    passed_counter: int = 0
 stat = Statistics()
 
 for extDir in extDirs:
-	filelist = os.listdir(extDir)
-	if not filelist:
-		continue
-	for file in os.listdir(extDir):
-		test_number = 0
-		test_number += 1
-		commandName = file.split('.')[0]
-		with open(f'{extDir}/{file}', 'r', encoding='utf-8') as asmFile:
-			asmText = asmFile.read()
-			fooPos = asmText.find('test')
-			res = asmText.find(commandName, fooPos)
-			falseRes = asmText.find(f'__{commandName}', fooPos) + 2 
-		#print(res)
-		stat.test_counter += 1
-		if res not in [-1, falseRes]:
-			print(Fore.GREEN + f'TEST #{test_number} PASSED: {commandName} is generated')
-			stat.passed_counter += 1
-		else:
-			print(Fore.RED + f'TEST #{test_number} FAILED: {commandName} is not supported')
-			print(Fore.RED + f'Error in file {extDir}/{file}')
-			#if falseResi:
-			#	print(Fore.RED + f'Used __{commandName}')
+    filelist = os.listdir(extDir)
+    if not filelist:
+        continue
+    for file in os.listdir(extDir):
+        test_number = 0
+        test_number += 1
+        commandName = file.split('.')[0]
+        with open(f'{extDir}/{file}', 'r', encoding='utf-8') as asmFile:
+            asmText = asmFile.read()
+            fooPos = asmText.find('test')
+            res = asmText.find(commandName, fooPos)
+            falseRes = asmText.find(f'__{commandName}', fooPos) + 2 
+        #print(res)
+        stat.test_counter += 1
+        if res not in [-1, falseRes]:
+            print(Fore.GREEN + f'TEST #{test_number} PASSED: {commandName} is generated')
+            stat.passed_counter += 1
+        else:
+            print(Fore.RED + f'TEST #{test_number} FAILED: {commandName} is not supported')
+            print(Fore.RED + f'Error in file {extDir}/{file}')
+            #if falseResi:
+            #	print(Fore.RED + f'Used __{commandName}')
 print(f'Stats: Passed {stat.passed_counter}/{stat.test_counter} tests')
