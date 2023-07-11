7b671ad4a83373f35153ac6a277965e459da32e2
Semyon Zernov <st095228@student.spbu.ru>
2023-07-11 05:15:42 +0300

    BUILD: added virtual environment in scripts (probably remove later)

--- run_me.py
+++ run_me.py
@@ -1,4 +1,20 @@
 import os
+import sys
+import subprocess
+
+
+# create virtual enviroment:
+venv_proc = subprocess.Popen([sys.executable, '-m', 'venv', '.env'])
+venv_proc.wait()
+
+venv_activate_proc = subprocess.Popen(['/bin/sh', '.env/bin/activate'], stdin=subprocess.PIPE, shell=True)
+venv_activate_proc.stdin.close()
+venv_activate_proc.wait()
+print('VENV activated')
+
+colorama_install_proc = subprocess.Popen([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
+colorama_install_proc.wait()
+
 
 os.system('python3 src/gen_asm.py')
 #os.wait()
