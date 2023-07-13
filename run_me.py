96817c92cab20b4c88d29bcb9e0a0ae2cf831e5f
Semyon Zernov <st095228@student.spbu.ru>
2023-07-13 11:52:50 +0300

    RUN_ME: fixed formatting

--- run_me.py
+++ run_me.py
@@ -25,4 +25,4 @@ os.system('python3 src/gen_asm.py')
 #os.wait()
 os.system('python3 src/run_tests.py')
 
-print('\nYou can check generated ASM in /tests/tmp')
+print('You can check generated ASM in /tests/tmp')
