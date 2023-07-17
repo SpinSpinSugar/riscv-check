d7edb590aa0b7ee3b527cebe33cfe9e2ae459fa5
Semyon Zernov <st095228@student.spbu.ru>
2023-07-17 23:26:31 +0300

    BUILD: refactored

--- clear_temps.py
+++ clear_temps.py
@@ -1,6 +1,14 @@
+#!/usr/bin/env python3
+"""Useful script to purge tmp"""
 import shutil
 import os
+import sys
 
-tmpDir = os.getcwd() + '/tests/tmp'
-print(tmpDir)
-shutil.rmtree(tmpDir)
+def main():
+    """Purge tmp dir"""
+    tmp_dir = os.getcwd() + '/tests/tmp'
+    print(tmp_dir)
+    shutil.rmtree(tmp_dir)
+
+if __name__ == '__main__':
+    sys.exit(main())
